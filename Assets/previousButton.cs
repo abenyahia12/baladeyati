1e93434fe63c319ddb3e230b0c2e7d4573ca54cb
abdelazizBenYahia <abenyahia@wpi.edu>
2021-01-07 19:35:50 +0100

    beta

---
+++ Assets/previousButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class previousButton : MonoBehaviour
+{
+    public ContentManager contentManager;
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                contentManager.SwitchCanvastoThemes();
+                contentManager.GenerateThemes();
+            }
+
+        }
+    }
+}
